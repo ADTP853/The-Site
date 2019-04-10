f9699ef8cb74de9a8796a70d57c5e4ac4db51a46
Alec Penney <ADTP853@Alecs-MacBook-Air.local>
2019-04-10 11:54:14 +0100

    initial commit

---
+++ JS/jsexample2.js
@@ -0,0 +1,33 @@
+var today = new Date();
+var dayNow = today.getDay();
+var day;
+
+if (dayNow = 0) 
+    {
+    day = 'How is your working week?';
+    }
+else if (dayNow = 1) 
+    {
+    day = 'How is your working week?';
+    }
+else if (dayNow = 2) 
+    {
+    day = 'How is your working week?';
+    }
+else if (dayNow = 3) 
+    {
+    day = 'How is your working week?';
+    }
+else if (dayNow = 4) 
+    {
+    day = 'How is your weekend?';
+    }
+else if (dayNow = 5) 
+    {
+    day = 'How is your weekend?';
+    }
+else if (dayNow = 6) 
+    {
+    day = 'How is your weekend?';
+    }
+document.write(day);
